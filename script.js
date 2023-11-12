b1dae8dff5ba0c91b5efdd5947d4fd2c0c73026c
DilankaSandeep <dilankacm@gmail.com>
2023-11-12 13:23:35 +0530

    Add swipper to section3

---
+++ script.js
@@ -0,0 +1,23 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1.5,
+    centeredSlides: true,
+    spaceBetween: 30,
+    autoplay: {
+        delay: 250,
+        disableOnInteraction: false,
+      },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+
+    loop: true,
+  });
+
+
+
+
+
+
+
+
